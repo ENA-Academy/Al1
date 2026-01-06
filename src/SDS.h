6d406c888fc71b10b423112d13ec12c93932be27
5938 <5938@Arpa.local>
2026-01-06 11:25:20 +0330

    compleatefirst

---
+++ src/SDS.h
@@ -0,0 +1,15 @@
+#ifndef SDS_H
+#define SDS_H
+
+#include <stdint.h>
+
+uint32_t sds32_round(uint32_t x);
+uint32_t sds32_round_inv(uint32_t y);
+
+
+//int  parse_bin32_msb_first(const char *s, uint32_t *out);
+//void print_bin32_msb_first(uint32_t v);
+
+#endif
+
+
---
+++ src/key.h
@@ -0,0 +1,3 @@
+/* key.c */
+void print_key_out(uint8_t KEY_OUT[26][((32/2)/2)]);
+int key_main(int argc, char *argv[], uint8_t out_key_out[26][((32/2)/2)]);
